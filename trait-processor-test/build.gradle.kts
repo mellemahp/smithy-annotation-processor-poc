890a4ead6e7ead7b61ec0214b8d9b0c7b8be2306
Hunter Mellema <mellemahp@gmail.com>
2023-10-31 14:32:26 -0600

    Switch to new gradle plugin version 0.9.0

--- trait-processor-test/build.gradle.kts
+++ trait-processor-test/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Test of Annotation processor that executes Trait code generation"
 
 plugins {
     id("smithy.annotation.processor.java-library-conventions")
-    id("software.amazon.smithy.gradle.smithy-jar") version "0.8.0"
+    id("software.amazon.smithy.gradle.smithy-jar") version "0.9.0"
 }
 
 dependencies {
@@ -14,5 +14,3 @@ dependencies {
 tasks.withType<JavaCompile>().configureEach {
     options.sourcepath = files(sourceSets["main"].java.srcDirs) + sourceSets["main"].resources.sourceDirectories
 }
-
-tasks["compileJava"].dependsOn("processResources")
